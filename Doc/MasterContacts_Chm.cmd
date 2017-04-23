e6d8aa211440046b8b01d84c93060e58d7516851
Master 2 CCI Tours <jf.pacory@laposte.net>
2017-04-23 17:31:21 +0200

    import initial

---
+++ Doc/MasterContacts_Chm.cmd
@@ -0,0 +1,9 @@
+﻿rem -------------------------------------------------------
+rem 15:24 03/01/2010 - générateur doc technique (jfp)
+rem -------------------------------------------------------
+
+rem SI intégration dans BUILD
+rem "C:\Program Files\EWSoftware\Sandcastle Help File Builder\SandcastleBuilderConsole.exe" $(SolutionDir)Doc\MasterClasses.shfb
+
+"C:\Program Files\EWSoftware\Sandcastle Help File Builder\SandcastleBuilderConsole.exe" MasterClasses.shfb
+
